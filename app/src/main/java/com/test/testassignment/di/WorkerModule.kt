c4ef5f892b3061dc2c9b3c3c222794a7e60c5dd3
mykola.puliaiev <mykola.puliaiev@gmail.com>
2023-06-30 15:47:14 +0200

    Created WorkerModule

---
+++ app/src/main/java/com/test/testassignment/di/WorkerModule.kt
@@ -0,0 +1,19 @@
+package com.test.testassignment.di
+
+import android.content.Context
+import androidx.work.WorkManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class WorkerModule {
+
+    @Singleton
+    @Provides
+    fun provideWorkManager(@ApplicationContext context: Context) = WorkManager.getInstance(context)
+}
