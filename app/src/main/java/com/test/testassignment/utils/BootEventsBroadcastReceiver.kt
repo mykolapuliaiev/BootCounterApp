d4263f5fae458aff1f417729bc937f222b4e6331
mykola.puliaiev <mykola.puliaiev@gmail.com>
2023-06-30 15:30:07 +0200

    Created broadcast receiver for boot events, registered in manifest

---
+++ app/src/main/java/com/test/testassignment/utils/BootEventsBroadcastReceiver.kt
@@ -0,0 +1,13 @@
+package com.test.testassignment.utils
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BootEventsBroadcastReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        // TODO: make action receiving
+    }
+}
