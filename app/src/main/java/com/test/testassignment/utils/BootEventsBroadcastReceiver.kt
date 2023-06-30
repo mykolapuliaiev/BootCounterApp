f1c0aa5669b26a509601e98282649591239b7a5c
mykola.puliaiev <mykola.puliaiev@gmail.com>
2023-06-30 15:35:01 +0200

    Added saving of boot action to database

--- app/src/main/java/com/test/testassignment/utils/BootEventsBroadcastReceiver.kt
+++ app/src/main/java/com/test/testassignment/utils/BootEventsBroadcastReceiver.kt
@@ -3,15 +3,27 @@ package com.test.testassignment.utils
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.test.testassignment.models.BootEvent
+import com.test.testassignment.repository.BootEventsRepository
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class BootEventsBroadcastReceiver: BroadcastReceiver() {
+class BootEventsBroadcastReceiver : BroadcastReceiver() {
+
+    @Inject
+    lateinit var repository: BootEventsRepository
     override fun onReceive(context: Context?, intent: Intent?) {
         val action = intent?.action
 
         if (action == Intent.ACTION_BOOT_COMPLETED) {
-            // todo: track event
+            CoroutineScope(Dispatchers.IO).launch {
+                val currentMillis = System.currentTimeMillis()
+                repository.addBootCompletedEvent(BootEvent(timestamp = currentMillis))
+            }
             // todo: show notification
         }
     }
