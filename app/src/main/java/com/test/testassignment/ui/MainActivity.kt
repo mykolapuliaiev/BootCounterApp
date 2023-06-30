b0a40429928e76939252730e343a7ed0e54ff04a
mykola.puliaiev <mykola.puliaiev@gmail.com>
2023-06-30 16:12:34 +0200

    Added displaying of boot events and notifications permission request

--- app/src/main/java/com/test/testassignment/ui/MainActivity.kt
+++ app/src/main/java/com/test/testassignment/ui/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.test.testassignment.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.test.testassignment.R
 import com.test.testassignment.models.BootEvent
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initPermissionLauncher()
+        requestNotificationPermission()
 
         viewModel.bootEventsLiveData.observe(this) {
             displayBootTimestamps(it)
@@ -28,7 +41,35 @@ class MainActivity : AppCompatActivity() {
         }
 
         textView.text = timestamps.withIndex().joinToString(separator = "\n") {
-            "${it.index + 1} - ${it.value}"
+            "${it.index + 1} - ${it.value.timestamp}"
+        }
+    }
+
+    private fun initPermissionLauncher() {
+        requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+                if (granted) {
+                    // notification permission granted
+                } else {
+                    // notification permission not granted
+                }
+            }
+    }
+
+    private fun requestNotificationPermission() {
+        if (
+            ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            // ready to show notifications
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        } else {
+            Toast.makeText(
+                this@MainActivity,
+                "Please, allow notification permission",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
